0e02cec39ac21d5121d34b2ffad2614af35aa07d
dislike_fruits <ryhaxa@gmail.com>
2025-12-17 15:57:32 +0900

    feat: トランザクション処理

--- internal/repository/tickets.go
+++ internal/repository/tickets.go
@@ -42,16 +42,26 @@ func (r *Repository) GetTickets(ctx context.Context) ([]*Ticket, error) {
 }
 
 func (r *Repository) CreateTicket(ctx context.Context, params CreateTicketParams) error {
-	if !(params.Status == "open" || params.Status == "in_progress" || params.Status == "closed") {
+	if !(params.Status == "not_planned" || params.Status == "not_written" || params.Status == "waiting_review" || params.Status == "waiting_sent" || params.Status == "sent" || params.Status == "milestone_scheduled" || params.Status == "completed" || params.Status == "forgotten") {
 		return fmt.Errorf("invalid status: %s", params.Status)
 	}
-	users, err := r.GetUsers(ctx)
+
+	unique_user_ids := make(map[string]struct{})
+	unique_user_ids[params.Assignee] = struct{}{}
+	for _, subAssignee := range params.SubAssignees {
+		unique_user_ids[subAssignee] = struct{}{}
+	}
+	for _, stakeholder := range params.Stakeholders {
+		unique_user_ids[stakeholder] = struct{}{}
+	}
+
+	users, err := r.GetUsers(ctx) // TODO: 全ユーザーを取得するのは効率が悪いので改善する
 	if err != nil {
 		return fmt.Errorf("failed to get users: %w", err)
 	}
 	assignee_found := false
 	for _, user := range users {
-		if user.Name == params.Assignee {
+		if user.ID == params.Assignee {
 			assignee_found = true
 			break
 		}
@@ -62,7 +72,7 @@ func (r *Repository) CreateTicket(ctx context.Context, params CreateTicketParams
 	for _, subAssignee := range params.SubAssignees {
 		sub_assignee_found := false
 		for _, user := range users {
-			if user.Name == subAssignee {
+			if user.ID == subAssignee {
 				sub_assignee_found = true
 				break
 			}
@@ -71,49 +81,53 @@ func (r *Repository) CreateTicket(ctx context.Context, params CreateTicketParams
 			return fmt.Errorf("sub-assignee not found: %s", subAssignee)
 		}
 	}
-	
-	_, err = r.db.ExecContext(ctx, `
-		INSERT INTO tickets (title, description, status, assignee, due)
-		VALUES (?, ?, ?, ?, ?)
+
+	tx, err := r.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+	defer tx.Rollback()
+
+	res, err := tx.ExecContext(ctx, `
+		INSERT INTO tickets (title, description, status, assignee, due) VALUES (?, ?, ?, ?, ?)
 	`, params.Title, params.Description, params.Status, params.Assignee, params.Due)
 	if err != nil {
 		return fmt.Errorf("failed to insert ticket: %w", err)
 	}
-	
-	ticket := new(Ticket)
-	if err := r.db.GetContext(ctx, ticket, "SELECT * FROM tickets WHERE id = LAST_INSERT_ID()"); err != nil {
-		return fmt.Errorf("failed to retrieve inserted ticket: %w", err)
+	ticketID, err := res.LastInsertId()
+	if err != nil {
+		return fmt.Errorf("failed to get last insert id: %w", err)
 	}
-	id := ticket.ID
-
+	
 	for _, subAssignee := range params.SubAssignees {
-		_, err = r.db.ExecContext(ctx, `
-			INSERT INTO ticket_sub_assignees (ticket_id, sub_assignee)
-			VALUES (?, ?)
-		`, id, subAssignee)
+		_, err = tx.ExecContext(ctx, `
+			INSERT INTO ticket_sub_assignees (ticket_id, sub_assignee) VALUES (?, ?)
+		`, ticketID, subAssignee)
 		if err != nil {
 			return fmt.Errorf("failed to insert sub-assignee: %w", err)
 		}
 	}
 
 	for _, stakeholder := range params.Stakeholders {
-		_, err = r.db.ExecContext(ctx, `
-			INSERT INTO ticket_stakeholders (ticket_id, stakeholder)
-			VALUES (?, ?)
-		`, id, stakeholder)
+		_, err = tx.ExecContext(ctx, `
+			INSERT INTO ticket_stakeholders (ticket_id, stakeholder) VALUES (?, ?)
+		`, ticketID, stakeholder)
 		if err != nil {
 			return fmt.Errorf("failed to insert stakeholder: %w", err)
 		}
 	}
 
 	for _, tag := range params.Tags {
-		_, err = r.db.ExecContext(ctx, `
-			INSERT INTO ticket_tags (ticket_id, tag)
-			VALUES (?, ?)
-		`, id, tag)
+		_, err = tx.ExecContext(ctx, `
+			INSERT INTO ticket_tags (ticket_id, tag) VALUES (?, ?)
+		`, ticketID, tag)
 		if err != nil {
 			return fmt.Errorf("failed to insert tag: %w", err)
 		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
 	}
 
 	return nil
