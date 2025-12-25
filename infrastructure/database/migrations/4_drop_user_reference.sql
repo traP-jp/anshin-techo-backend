cbb6f613bb008d0119fb55c6ada38cc65891ae0c
dislike_fruits <ryhaxa@gmail.com>
2025-12-25 20:35:29 +0900

    fix: 外部キー制約の命名を修正

--- infrastructure/database/migrations/4_drop_user_reference.sql
+++ infrastructure/database/migrations/4_drop_user_reference.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 
 ALTER TABLE notes
-  DROP FOREIGN KEY `2`;
+  DROP FOREIGN KEY `fk_notes_author`;
 
 ALTER TABLE note_review_assignees
-  DROP FOREIGN KEY `2`;
+  DROP FOREIGN KEY `fk_note_review_assignees_assignee`;
 
 ALTER TABLE reviews
-  DROP FOREIGN KEY `2`;
+  DROP FOREIGN KEY `fk_reviews_author`;
