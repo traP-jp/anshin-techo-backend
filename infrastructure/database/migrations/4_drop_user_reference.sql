5a8f11f5c8daa96c120a3005c5b2d2b30ac2f6aa
dislike_fruits <ryhaxa@gmail.com>
2025-12-25 20:41:09 +0900

    fix: goose Down

--- infrastructure/database/migrations/4_drop_user_reference.sql
+++ infrastructure/database/migrations/4_drop_user_reference.sql
@@ -8,3 +8,13 @@ ALTER TABLE note_review_assignees
 
 ALTER TABLE reviews
   DROP FOREIGN KEY `fk_reviews_author`;
+
+-- +goose Down
+ALTER TABLE notes
+  ADD CONSTRAINT `fk_notes_author` FOREIGN KEY (author) REFERENCES users(traq_id);
+
+ALTER TABLE note_review_assignees
+  ADD CONSTRAINT `fk_note_review_assignees_assignee` FOREIGN KEY (assignee) REFERENCES users(traq_id);
+
+ALTER TABLE reviews
+  ADD CONSTRAINT `fk_reviews_author` FOREIGN KEY (author) REFERENCES users(traq_id);
